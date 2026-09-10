020ed3244402fb69c94e93b1b17870e1a3f1d983
schoolinfogade-prog <schoolinfogade@gmail.com>
2026-09-09 22:29:09 -0400

    sep9th

--- tests/test_datahandling.py
+++ tests/test_datahandling.py
@@ -58,6 +58,6 @@ def test_CLI():
     print(usname11)
 
 def test_json_file_read():
-    data = jsonhandling1("testdata\\creds.json")
+    data = jsonhandling1("testdata\creds.json")
     assert data["positive"]["iphone1"] == "iphone7"
     assert data["positive"]["iphone2"] == "iphone8"
